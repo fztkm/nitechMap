7371824b3906cb51b7a1e074e0a081ee301fc8fa
fztkm <forthune.0517@gmail.com>
2021-08-19 22:56:46 +0900

    時間割編集の為のbottom sheet を追加

--- lib/screens/timetable_screen.dart
+++ lib/screens/timetable_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import '../widgets/edit_card.dart';
 
 class TimeTableScreen extends StatelessWidget {
   static String id = 'timetabele_screen';
   static var weekdays = ['月', '火', '水', '木', '金'];
 
   final timeTabel = ['微積', '線形代数', 'プログラミング', 'コンピュータアーキテクチャ', 'abc'];
+
+  void startEditing(BuildContext context) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (cx) {
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: EditCard(),
+        );
+      },
+    );
+  }
 
   List<Expanded> classroomCards() {
     return List.generate(
@@ -44,31 +58,41 @@ class TimeTableScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('時間割'),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8),
-            child: TextButton.icon(
-              onPressed: () {},
-              icon: Icon(
-                Icons.edit,
-                color: Colors.white,
-              ),
-              label: Text(
-                'Edit',
-                style: TextStyle(color: Colors.white),
-              ),
+    AppBar appBar = AppBar(
+      title: Text('時間割'),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 8),
+          child: TextButton.icon(
+            onPressed: () {
+              startEditing(context);
+            },
+            icon: Icon(
+              Icons.edit,
+              color: Colors.white,
+            ),
+            label: Text(
+              'Edit',
+              style: TextStyle(color: Colors.white),
             ),
           ),
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          ...tiles(classroomCards(), context),
-        ],
+        ),
+      ],
+    );
+    return Scaffold(
+      appBar: appBar,
+      body: SingleChildScrollView(
+        child: Container(
+          height: (MediaQuery.of(context).size.height -
+              appBar.preferredSize.height -
+              MediaQuery.of(context).padding.top),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ...tiles(classroomCards(), context),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/edit_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class EditCard extends StatefulWidget {
+  const EditCard({Key? key}) : super(key: key);
+
+  @override
+  _EditCardState createState() => _EditCardState();
+}
+
+class _EditCardState extends State<EditCard> {
+  Container inputField() {
+    return Container(
+        padding: EdgeInsets.only(top: 10, bottom: 10, right: 20),
+        width: MediaQuery.of(context).size.width * 0.65,
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: '講義'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: '講義室番号'),
+            ),
+          ],
+        ));
+  }
+
+  List<Container> inputFieldList() {
+    return List.generate(5, (index) {
+      return inputField();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+          alignment: Alignment.centerRight,
+          child: Column(children: inputFieldList()),
+        ),
+      ),
+    );
+  }
+}
