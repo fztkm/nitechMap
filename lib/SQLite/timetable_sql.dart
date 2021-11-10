5d60b70cbd5fc809847ae644f6c01031b73eb35d
fztkm <forthune.0517@gmail.com>
2021-11-10 11:39:41 +0900

    データベースから取得してTimeTableに変換

---
+++ lib/SQLite/timetable_sql.dart
@@ -0,0 +1,3 @@
+class TimeTableDatabese{
+  // static Future<void> insertTimeTable()
+}
--- lib/providers/timetable.dart
+++ lib/providers/timetable.dart
@@ -7,6 +7,7 @@ class ClassData {
   String classroom = '0000';
 
   ClassData(this.className);
+  ClassData.setRoom(this.className, this.classroom);
 
   void setClassroom(String room) {
     classroom = room;
@@ -25,6 +26,38 @@ enum DayOfWeek {
   Fry,
 }
 
+int _dayOfWeekToInt(DayOfWeek day) {
+  switch (day) {
+    case DayOfWeek.Mon:
+      return 0;
+    case DayOfWeek.Tue:
+      return 1;
+    case DayOfWeek.Wed:
+      return 2;
+    case DayOfWeek.Thu:
+      return 3;
+    case DayOfWeek.Fry:
+      return 4;
+  }
+}
+
+DayOfWeek? _intToDayOfWeek(int day) {
+  switch (day) {
+    case 0:
+      return DayOfWeek.Mon;
+    case 1:
+      return DayOfWeek.Tue;
+    case 2:
+      return DayOfWeek.Wed;
+    case 3:
+      return DayOfWeek.Thu;
+    case 4:
+      return DayOfWeek.Fry;
+    default:
+      return null;
+  }
+}
+
 class TimeTable with ChangeNotifier {
   //Map<String, List<0 or ClassData> >
   Map<DayOfWeek, List<dynamic>> _timetable = {
@@ -37,10 +70,6 @@ class TimeTable with ChangeNotifier {
 
   var database;
 
-  TimeTable() {
-    database = openTimeTableDatabase();
-  }
-
   Map<DayOfWeek, List<dynamic>> timetable() {
     return _timetable;
   }
@@ -50,14 +79,68 @@ class TimeTable with ChangeNotifier {
     notifyListeners();
   }
 
-  //データベースに接続
+  //一番最初にやる。データベースに接続する
+  Future<void> getinitDatabase() async {
+    database = await openTimeTableDatabase();
+  }
+
+  //データベースに接続 getinitDatabaseでdatabase変数に代入する
   Future<Database> openTimeTableDatabase() async {
     return openDatabase(
       join(await getDatabasesPath(), 'timetable_databese.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE timetable(day INTEGER, time INTEGER, name TEXT, room TEXT)",
+        );
+      },
+      version: 1,
     );
   }
 
-  Future<void> insertTimeable(DayOfWeek day, List<dynamic> newTT) {
-    return Future.delayed(Duration.zero);
+  //データベースに挿入
+  Future<void> insertTimeable(DayOfWeek day, List<dynamic> newTT) async {
+    final Database db = await database;
+    int dayInt = _dayOfWeekToInt(day);
+    for (int i = 0; i < 5; i++) {
+      await db.insert(
+        'timetable',
+        timeTableListPerDoWToMap(dayInt, i, newTT),
+        conflictAlgorithm: ConflictAlgorithm.replace, //上書きする
+      );
+    }
+  }
+
+  //n DayOfWeekを整数にして渡す
+  Map<String, dynamic> timeTableListPerDoWToMap(
+      int day, int time, List<dynamic> tt) {
+    return {
+      "day": day,
+      "time": time,
+      "name": tt[time] == 0 ? 0 : (tt[time] as ClassData).className,
+      "room": tt[time] == 0 ? 0 : (tt[time] as ClassData).classroom,
+    };
+  }
+
+  //全データをセット 授業単位で作られたmapのリスト keys= {"day", "time", "name", "room"}
+  void setAllData(List<Map<String, dynamic>> maps) {
+    maps.forEach((table) {
+      int day = int.parse(table["day"]);
+      int time = int.parse(table["time"]);
+      var classDataOrZero;
+      if (table["name"] != 0 && table["room"] != 0) {
+        classDataOrZero = ClassData.setRoom(table["name"], table["room"]);
+      } else {
+        classDataOrZero = 0;
+      }
+      _timetable[_intToDayOfWeek(day)]![time] = classDataOrZero;
+    });
+  }
+
+  //データの取得
+  Future<TimeTable> getTimeTable() async {
+    final List<Map<String, dynamic>> maps = await database.query("timetable");
+    TimeTable fetchedTimeTable = TimeTable();
+    fetchedTimeTable.setAllData(maps);
+    return fetchedTimeTable;
   }
 }
