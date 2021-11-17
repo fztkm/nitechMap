8f772f225415603bf81b85f5fc6f87e9206451a7
fztkm <forthune.0517@gmail.com>
2021-11-17 13:39:56 +0900

    ライセンス表示

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nitechmap_c0de/screens/map_screen.dart';
 import 'package:nitechmap_c0de/screens/timetable_screen.dart';
+import 'package:package_info/package_info.dart';
 
 class MainDrawer extends StatelessWidget {
   const MainDrawer({Key? key}) : super(key: key);
@@ -50,6 +51,31 @@ class MainDrawer extends StatelessWidget {
                   .textTheme
                   .headline6!
                   .copyWith(color: Theme.of(context).primaryColor),
+            ),
+          ),
+          Divider(
+            thickness: 1.0,
+          ),
+          TextButton.icon(
+            onPressed: () async {
+              final info = await PackageInfo.fromPlatform();
+              showLicensePage(
+                context: context,
+                applicationName: info.appName,
+                applicationVersion: info.version,
+                applicationIcon: Icon(Icons.tag_faces),
+                applicationLegalese: "ほげほげ",
+              );
+            },
+            icon: Icon(
+              Icons.info,
+              size: 30,
+            ),
+            label: Text(
+              'Licenses',
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: Colors.grey,
+                  ),
             ),
           ),
         ],
